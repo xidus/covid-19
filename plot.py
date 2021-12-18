bc426df1e854561bf0b2361c1d4e384a62683b64
xidus <xidus@users.noreply.github.com>
2021-12-18 20:28:32 +0100

    Highlight nearest point.

--- plot.py
+++ plot.py
@@ -33,23 +33,51 @@ g = g.reset_index('yearweek')
 # Rename columns after aggregation
 g = g.rename(columns=dict(infected_today='infected_this_week_so_far'))
 
-base = alt.Chart(g).encode(
+highlight = alt.selection(
+    type='single',
+    on='mouseover',
+    fields=['symbol'],
+    nearest=True,
+)
+
+base_weekly = alt.Chart(g).encode(
     x=alt.X('infected_accum:Q', scale=alt.Scale(type='log')),
     y=alt.X('infected_this_week_so_far:Q', scale=alt.Scale(type='log')),
+).properties(width=700, height=400).interactive()
+weekly = base_weekly.mark_circle().encode(
+    opacity=alt.value(0)
+).add_selection(
+    highlight
+)
+weekly += base_weekly.mark_line()
+weekly += base_weekly.mark_point(
+    size=alt.condition(~highlight, alt.value(1), alt.value(3)),
     tooltip=[
         'yearweek:N',
         'infected_accum:Q',
         'infected_this_week_so_far:Q',
     ],
-).properties(width=700, height=400)
-chart = base.mark_line()
-chart += base.mark_point()
-chart = chart.interactive()
+)
 
-chart &= alt.Chart(df).mark_line().encode(
+base_daily = alt.Chart(df).encode(
     x=alt.X('infected_accum:Q'),
     y=alt.X('infected_today:Q'),
-    tooltip='date:T',
-).properties(width=700, height=400).interactive()
 
+).properties(width=700, height=400).interactive()
+daily = base_daily.mark_circle().encode(
+    opacity=alt.value(0)
+).add_selection(
+    highlight
+)
+daily += base_daily.mark_line()
+daily += base_daily.mark_point(
+    size=alt.condition(~highlight, alt.value(1), alt.value(3)),
+    tooltip=[
+        'yearweek:N',
+        'infected_accum:Q',
+        'infected_today:Q',
+    ],
+)
+
+chart = weekly & daily
 chart.save('index.html')
